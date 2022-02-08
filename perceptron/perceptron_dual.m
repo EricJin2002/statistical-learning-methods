1897d14012ecb913278485d9a001214edeb9b347
EricJin2002 <769327841@qq.com>
2022-02-08 17:32:59 +0800

    第2章感知机

---
+++ perceptron/perceptron_dual.m
@@ -0,0 +1,19 @@
+function [w,b,a] = perceptron_dual(x,y,eta)
+    [n,N]=size(x);
+    a=zeros(1,N);
+    b=0;
+    %Gram matrix
+    G=x'*x;
+    flag=true;
+    while flag
+        flag=false;
+        for i=1:N
+            if(y(i)*(a.*y*G(:,i)+b)<=0)
+                flag=true;
+                a(i)=a(i)+eta;
+                b=b+eta*y(i);
+            end
+        end
+    end
+    w=x*(a.*y)';
+end
---
+++ perceptron/perceptron_test.m
@@ -0,0 +1,9 @@
+clear;clc;
+x=[1 2 3 3 3 4 5 3 4 2 ;
+   3 2 4 5 2 3 4 1 2 2];
+y=[1 1 -1 -1 1 -1 -1 1 1 1];
+scatter(x(1,y==1),x(2,y==1),'r')
+hold on
+scatter(x(1,y==-1),x(2,y==-1),'b')
+[w,b,a]=perceptron_dual(x,y,1)
+fimplicit(@(x1,x2)w'*[x1;x2]+b,[0 5])
